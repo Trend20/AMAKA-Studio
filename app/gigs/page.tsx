b01f2b14c248003d4af4929db6ce451a452658df
Trend20 <enockomondi305@gmail.com>
2024-05-03 18:18:08 +0300

    padding fix on single gigs

--- app/gigs/page.tsx
+++ app/gigs/page.tsx
@@ -5,7 +5,7 @@ import GigDetails from "@/app/gigs/components/GigDetails";
 const GigsPage = () =>{
     return(
         <div className="flex w-full px-4">
-            <div className="flex flex-col w-1/2 space-y-8 border-r border-stroke py-4">
+            <div className="flex flex-col w-1/2 space-y-8 border-r border-stroke py-2">
                 {
                     gigsData.map((item)=>(
                         <SingleGig gig={item} key={item.id} />
